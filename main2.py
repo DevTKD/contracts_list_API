74ce0dc5d2bb5bf160156a64a0b05c045391fc8e
Tanya Davis <tdavis_la@outlook.com>
2026-05-19 21:12:29 -0400

    Added Pydantic and begain establishing field validations.

--- main2.py
+++ main2.py
@@ -1,4 +1,8 @@
+from datetime import date
+from typing import Literal
+
 from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app  = FastAPI()
 
@@ -6,9 +10,9 @@ class Contract:
     contract_id: str
     contract_name: str
     contract_type: str
-    contract_status: str
-    contract_start_date: str
-    contract_end_date: str
+    contract_status: Literal["active", "inactive"]
+    contract_start_date: date
+    contract_end_date: date
 
     def __init__(self, contract_id, contract_name, contract_type, contract_status, contract_start_date,
                  contract_end_date):
@@ -19,6 +23,16 @@ class Contract:
         self.contract_start_date = contract_start_date
         self.contract_end_date = contract_end_date
 
+# ToDo: Update contract_id to an optional
+class ContractRequest(BaseModel):
+    contract_id: str
+    contract_name: str = Field(min_length=3)
+    contract_type: str = Field(min_length=3)
+    contract_status: Literal["active", "inactive"]
+    contract_start_date: date
+    contract_end_date: date
+
+# ToDo: fix dates in CONTRACTS
 CONTRACTS = [
     Contract("CT001", "Bilateral NDA", "NDA", "Active", "01/20/2025", "01/19/2027"),
     Contract("CT002", "Master Services Agreement", "MSA", "Active", "06/18/2022", "06/17/2027"),
@@ -32,3 +46,4 @@ CONTRACTS = [
 async def get_all_contracts():
     return CONTRACTS
 
+# ToDo: add post endpoint and function around automatic numbering of the contract IDs
